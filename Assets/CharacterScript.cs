3dd12b6427e927eab512fe559b980b16ba4a9a48
HWCI <howardchoi7@hotmail.com>
2018-12-27 01:01:23 +0800

    Backup

---
+++ Assets/CharacterScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
+
+public class CharacterScript : MonoBehaviour, IPointerClickHandler
+{
+
+	private Transform _transform;
+	private Animator _anim;
+	private NavMeshAgent navAgent;
+	// Use this for initialization
+	void Start ()
+	{
+		this.navAgent = this.gameObject.GetComponent<NavMeshAgent>();
+		_transform = this.gameObject.GetComponent<Transform>();
+		_anim = this.gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		_anim.SetFloat("Speed", navAgent.velocity.magnitude);
+		if (_anim.GetFloat("Speed") <= 0.1)
+		{
+			_anim.SetBool("Rest", true);
+		}
+		else
+		{
+			_anim.SetBool("Rest", false);
+		}
+	}
+
+	public void Move(Transform target)
+	{
+		navAgent.Move(target.position);
+	}
+
+	public void Skill1()
+	{
+		
+	}
+
+	public void Skill2()
+	{
+		
+	}
+
+	public void Skill3()
+	{
+		
+	}
+
+	public void SetChara()
+	{
+		MovementController.instance.SetChara(this);
+		Debug.Log("Clicked");
+	}
+	public void OnPointerClick(PointerEventData pED)
+	{
+		MovementController.instance.SetChara(this);
+		Debug.Log("Clicked");
+	}
+
+	public void Move(Vector3 location)
+	{
+		navAgent.destination = location;
+	}
+}
---
+++ Assets/MovementController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MovementController : MonoBehaviour
+{
+
+	public static MovementController instance;
+
+	public CharacterScript _chara;
+
+	public Transform _target;
+	// Use this for initialization
+	void Start ()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+		if (instance != this)
+		{
+			DestroyImmediate(this.gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			RaycastHit hit;
+                
+			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
+				Debug.Log(hit.collider.gameObject.name);
+				if (hit.collider.gameObject.CompareTag("Grid"))
+				{
+					if (_chara != null)
+					{
+						_chara.gameObject.GetComponent<NavMeshAgent>().destination = hit.transform.position;
+					}
+				}
+				if (hit.collider.gameObject.CompareTag("Player"))
+				{
+					_chara = hit.transform.GetComponent<CharacterScript>();
+				}
+			}
+		}
+	}
+
+	public void SetChara(CharacterScript chara)
+	{
+		_chara = chara;
+	}
+
+	public void SetTarget(Transform target)
+	{
+		_target = target;
+		Move();
+	}
+
+	public void Move()
+	{
+		if (_chara != null && _target != null)
+		{
+			_chara.Move(_target);
+		}
+	}
+}
