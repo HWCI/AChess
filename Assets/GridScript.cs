63befc2d227967b985aab3d35e01be687cc46145
ANI_SPC05\Staff <aaa@cccs.com>
2018-12-27 11:37:28 +0800

    fix

---
+++ Assets/GridScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GridScript : MonoBehaviour, IPointerClickHandler
+{
+	private Collider collider;
+	public delegate void onTouchTrigger ();
+	public event onTouchTrigger onTouchEvents;
+
+	void Start()
+	{
+		collider = this.gameObject.GetComponent<Collider>();
+	}
+
+	void Update()
+	{
+		
+	}
+
+	public void SetTarget()
+	{
+		MovementController.instance.SetTarget(this.transform);
+		Debug.Log("Clicked");
+	}
+
+	public void OnPointerClick(PointerEventData pED)
+	{
+		MovementController.instance.SetTarget(this.transform);
+		Debug.Log("Clicked");
+	}
+}
