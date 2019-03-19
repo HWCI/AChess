464d89c683eba5cf18714a457f4aca848ace1405
HWCI <howardchoi7@gmail.com>
2019-03-19 18:39:41 +0800

    19/3

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -17,17 +17,76 @@ public class UIManager : MonoBehaviour
     public Button Skill3;
     public Button EndTurn;
     public Button Reset;
+
+    public static UIManager instance;
     
     
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        if (instance != this) DestroyImmediate(gameObject);
     }
-
     // Update is called once per frame
     void Update()
     {
         
     }
+
+    public void Onclick(Button btn)
+    {
+        if (btn == Skill1)
+        {
+            
+        }if (btn == Skill2)
+        {
+            
+        }if (btn == Skill3)
+        {
+            
+        }if (btn == EndTurn)
+        {
+            
+        }if (btn == Reset)
+        {
+            
+        }
+    }
+    public void SetSkill1Txt(string txt)
+    {
+        skill1txt.text = txt;
+    }
+    public void SetSkill2Txt(string txt)
+    {
+        skill2txt.text = txt;
+    }
+    public void SetSkill3Txt(string txt)
+    {
+        skill3txt.text = txt;
+    }
+    public void SetPhaseTxt(string txt)
+    {
+        skill3txt.text = txt;
+    }
+
+    public void ShowSkill1Button(bool _show)
+    {
+        Skill1.gameObject.SetActive(_show);
+    }
+    public void ShowSkill2Button(bool _show)
+    {
+        Skill2.gameObject.SetActive(_show);
+    }
+    public void ShowSkill3Button(bool _show)
+    {
+        Skill3.gameObject.SetActive(_show);
+    }
+    public void ShowTurnEndButton(bool _show)
+    {
+        EndTurn.gameObject.SetActive(_show);
+    }
+    
 }
